ee43d0fc64953c453cbffc85b6e47107321f357a
codingfullon <kavitaagrawal001@mail.com>
2017-10-23 11:01:01 -0700

    Add weekly habits window added with ListView.

--- app/src/main/java/com/example/kavitapc/fitnessreminder/AddNewGoals.java
+++ app/src/main/java/com/example/kavitapc/fitnessreminder/AddNewGoals.java
@@ -12,42 +12,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AddNewGoals.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AddNewGoals#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AddNewGoals extends Fragment implements View.OnClickListener {
+public class AddNewGoals extends Fragment {
 
     private Toast mToast;
     private OnFragmentInteractionListener mListener;
-    private TextView mTextView;
+    private TextView tvDailyHabits;
+    private TextView tvWeeklyHabits;
+    private TextView tvMonthlyHabits;
+    private TextView tvCreateOwn;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    // TODO: Rename and change types of parameters
+    // TODO: +]
+    // Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-
 
     public AddNewGoals() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddNewGoals.
-     */
     // TODO: Rename and change types and number of parameters
     public static AddNewGoals newInstance(String param1, String param2) {
         AddNewGoals fragment = new AddNewGoals();
@@ -69,17 +55,48 @@ public class AddNewGoals extends Fragment implements View.OnClickListener {
 
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_new_goals, container, false);
-        mTextView = (TextView)view.findViewById(R.id.tvDailyHabits);
-        mTextView.setOnClickListener(this);
+        tvDailyHabits = (TextView)view.findViewById(R.id.tvDailyHabits);
+        tvWeeklyHabits = (TextView)view.findViewById(R.id.tvWeeklyHabits);
+        tvMonthlyHabits = (TextView)view.findViewById(R.id.tvMonthlyHabits);
+        tvCreateOwn = (TextView)view.findViewById(R.id.tvCreateOwn);
+
+        TextView[] textViews = {tvDailyHabits, tvWeeklyHabits, tvMonthlyHabits, tvCreateOwn};
+        for(TextView tv: textViews) {
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int id = v.getId();
+                    Context context = getContext();
+                    if (id == R.id.tvDailyHabits) {
+                        mToast = Toast.makeText(context, "Item 1 was clicked", Toast.LENGTH_LONG);
+                        mToast.show();
+                        Intent intent = new Intent(context, DailyHabitsDetails.class);
+                        startActivity(intent);
+                    } else if (id == R.id.tvWeeklyHabits) {
+                        mToast = Toast.makeText(context, "Item 2 was clicked", Toast.LENGTH_LONG);
+                        mToast.show();
+                    } else if (id == R.id.tvMonthlyHabits) {
+                        mToast = Toast.makeText(context, "Item 3 was clicked", Toast.LENGTH_LONG);
+                        mToast.show();
+
+                    } else if (id == R.id.tvCreateOwn) {
+                        mToast = Toast.makeText(context, "Item 4 was clicked", Toast.LENGTH_LONG);
+                        mToast.show();
+
+                    }
+                }
+            });
+        }
         return view;
     }
-
 
 
     @Override
@@ -99,44 +116,6 @@ public class AddNewGoals extends Fragment implements View.OnClickListener {
         mListener = null;
     }
 
-
-
-
-    @Override
-    public void onClick (final View v){
-        int id = v.getId();
-        Context context = getContext();
-        if (id == R.id.tvDailyHabits) {
-            mToast = Toast.makeText(context, "Item was clicked", Toast.LENGTH_LONG);
-            mToast.show();
-            Intent intent = new Intent(context, DailyHabitsDetails.class);
-            startActivity(intent);
-        } else if (id == R.id.tvWeeklyHabits) {
-            mToast = Toast.makeText(context, "Item was clicked", Toast.LENGTH_LONG);
-            mToast.show();
-        } else if (id == R.id.tvMonthlyHabits) {
-            mToast = Toast.makeText(context, "Item was clicked", Toast.LENGTH_LONG);
-            mToast.show();
-
-        } else if (id == R.id.tvCreateOwn) {
-            mToast = Toast.makeText(context, "Item was clicked", Toast.LENGTH_LONG);
-            mToast.show();
-
-        }
-
-    }
-
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
--- app/src/main/java/com/example/kavitapc/fitnessreminder/DailyHabitsDetails.java
+++ app/src/main/java/com/example/kavitapc/fitnessreminder/DailyHabitsDetails.java
@@ -1,15 +1,24 @@
 package com.example.kavitapc.fitnessreminder;
+//Shows daily habits list to add
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -18,49 +27,53 @@ import java.util.List;
 
 public class DailyHabitsDetails extends AppCompatActivity {
 
-    private CheckBox cbWaterReminder;
-    private CheckBox cbExerciseReminder;
-    private CheckBox cbFruitsReminder;
-    private CheckBox cbYogaReminder;
-
+   private ListView lvDailyHabits;
+   private ListViewCustomAdapter customAdapter;
+   ArrayList<ItemAttributes> dailyHabitsArray = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_habits_details);
+        lvDailyHabits = (ListView) findViewById(R.id.simpleListView);
 
-        cbWaterReminder = (CheckBox) findViewById(R.id.cbWaterReminder);
-        cbExerciseReminder = (CheckBox) findViewById(R.id.cbExerciseReminder);
-        cbFruitsReminder = (CheckBox) findViewById(R.id.cbFruitsReminder);
-        cbYogaReminder = (CheckBox) findViewById(R.id.cbYogaReminder);
-
-        CheckBox[] checkBoxes = {cbWaterReminder, cbExerciseReminder, cbFruitsReminder, cbYogaReminder};
-
-        for (CheckBox cb : checkBoxes) {
-            cb.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if ((cbWaterReminder).isChecked()) {
-                        Log.i("checkbox 1", "clicked");
-                        createPreferences("cbWaterReminder", true);
-                    }
-                    if ((cbExerciseReminder).isChecked()) {
-                        Log.i("checkbox 2", "clicked");
-                        createPreferences("cbExerciseReminder", true);
-                    }
-                    if ((cbFruitsReminder).isChecked()) {
-                        Log.i("checkbox 3", "clicked");
-                        createPreferences("cbFruitsReminder", true);
-                    }
-                    if ((cbYogaReminder).isChecked()) {
-                        Log.i("checkbox 4", "clicked");
-                        createPreferences("cbYogaReminder", true);
-                    }
-                }
-            });
-        }
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.WaterReminder), R.drawable.ic_drink_water_black_24dp));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.YogaReminder), R.drawable.ic_yoga_black_24dp));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.ExerciseReminder), R.drawable.ic_exercise_black_24dp));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.FruitsReminder), R.drawable.ic_fruits_black_24dp));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.ExpressGratitude), R.drawable.ic_express_grattitude_black_24dp));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.LaughLaughAndLaugh), R.drawable.ic_sentiment_laugh_satisfied_black_24dp));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.Read), R.drawable.ic_read_book_black_24dp));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.WriteSomething), R.drawable.ic_write_black_24dp));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.WorkOnSecretProject), R.drawable.ic_secret_agent_black_24dp));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.Running), R.drawable.ic_directions_run_black_24dp));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.Meditate), R.drawable.ic_meditation_yoga_posture));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.LearnNewWords), R.drawable.ic_book_black_24dp));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.EatBowlSalad), R.drawable.ic_fruits_black_24dp));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.Study), R.drawable.ic_read_book_black_24dp));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.CleanTidy), R.drawable.ic_wiping_swipe_clean_tidy));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.CheckToDo), R.drawable.ic_book_black_24dp));
+        dailyHabitsArray.add(new ItemAttributes(getString(R.string.Breakfast), R.drawable.ic_fruits_black_24dp));
 
 
+        customAdapter = new ListViewCustomAdapter(this, R.layout.daily_habits_rows, dailyHabitsArray);
+        lvDailyHabits.setAdapter(customAdapter);
+
+        lvDailyHabits.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                View view2 = customAdapter.getView(position, view, parent);
+                CheckBox checkBox = (CheckBox) view.findViewById(R.id.cbDailyHabitsRow);
+                checkBox.setChecked(true);
+                checkBox.setFocusable(true);
+                checkBox.setFocusableInTouchMode(true);
+                String text = checkBox.getText().toString();
+                Toast.makeText(getApplicationContext(), "Selected item: " + text + " - " + position, Toast.LENGTH_SHORT).show();
+                Log.i(text, "clicked");
+
+            }
+        });
 
     }
 
@@ -72,5 +85,6 @@ public class DailyHabitsDetails extends AppCompatActivity {
         editor.commit();
 
     }
+
 
 }
