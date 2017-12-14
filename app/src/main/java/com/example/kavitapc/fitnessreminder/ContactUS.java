4c22d9c920eaed0230c5745ab7e94e3b1bd2dbe0
codingfullon <kavitaagrawal001@mail.com>
2017-12-14 15:24:28 -0800

    1) Onboarding Activity 2) App Tour 3) ContactUS 4)Fixed daily progress data

---
+++ app/src/main/java/com/example/kavitapc/fitnessreminder/ContactUS.java
@@ -0,0 +1,31 @@
+package com.example.kavitapc.fitnessreminder;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ContactUS extends AppCompatActivity {
+    String textData;
+    EditText mailText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+         mailText = findViewById(R.id.editTextEmailBody);
+
+
+    }
+
+    public void sendEmail(View view){
+        textData = mailText.getText().toString();
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto","kavitaagrawal001@gmail.com", null)); //TODO:change the id to your email id
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Mail from ActFeed");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, textData);
+        startActivity(Intent.createChooser(emailIntent, "Send email..."));
+    }
+}
